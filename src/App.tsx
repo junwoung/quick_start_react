531d47ea9b0e0bcedbc954007ebd42da326f0730
wangjun.byte <wangjun.byte@bytedance.com>
2022-07-25 16:54:27 +0800

    feat: 添加prettier配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import { Index } from './pages/Index';
+import { Home } from './pages/Home';
 
 function App() {
-  return <div className="mt-1">hello</div>;
+  return (
+    <div className="p-2">
+      <Index />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Index/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Index: React.FC = () => {
+  var var1 = '1222';
+
+  return <div className="text-red-600 font-bold">这是一个引入了tailwindcss的模板项目</div>;
+};
